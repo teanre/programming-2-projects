7391b55f8f154e379097096f065e618f68b20ef7
Terhi Rees <terhi.rees@tuni.fi>
2022-01-14 14:11:54 +0200

    missing dot added

--- student/02/cube/main.cpp
+++ student/02/cube/main.cpp
@@ -13,12 +13,12 @@ int main()
 
     if (cube < 0){
         cout << "Error! The cube of " << number << " is not " <<
-                cube << endl;
+                cube << "." << endl;
     } else if (cube / number / number != number){
         cout << "Error! The cube of " << number << " is not " <<
-                cube << endl;
+                cube << "." << endl;
     }else {
-    cout << "The cube of " << number << " is " << cube << endl;
+    cout << "The cube of " << number << " is " << cube << "." << endl;
     }
 
     return 0;
