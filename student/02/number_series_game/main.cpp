6950d173325c160309b1e35864127b50ce5a3b4f
Terhi Rees <terhi.rees@tuni.fi>
2022-01-14 11:28:16 +0200

    Syötteen lukeminen ja numeroiden tulostus

--- student/02/number_series_game/main.cpp
+++ student/02/number_series_game/main.cpp
@@ -3,7 +3,13 @@
 
 int main()
 {
+    int nr_count = 0;
     std::cout << "How many numbers would you like to have? ";
+    std::cin >> nr_count;
+
+    for (int number = 1; number < nr_count+1; ++number) {
+        std::cout << number << std::endl;
+    }
 
     return 0;
 }
