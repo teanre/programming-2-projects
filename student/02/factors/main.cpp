8e11850cafc39cd64e5b1e38362c02579eba8678
Terhi Rees <terhi.rees@tuni.fi>
2022-01-14 13:09:08 +0200

    ei hyväksy nollaa tai negatiivisia lukuja

--- student/02/factors/main.cpp
+++ student/02/factors/main.cpp
@@ -4,7 +4,13 @@ using namespace std;
 
 int main()
 {
+    int number;
     cout << "Enter a positive number: ";
+    cin >> number;
+
+    if (number <= 0){
+        cout << "Only positive numbers accepted" << endl;
+    }
 
     return 0;
 }
