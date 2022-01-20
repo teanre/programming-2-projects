3c40851f46a3aae25bd8c12b92edcfad878b3315
Terhi Rees <terhi.rees@tuni.fi>
2022-01-20 13:07:32 +0200

    corrected error msg

--- student/02/encryption/main.cpp
+++ student/02/encryption/main.cpp
@@ -11,7 +11,7 @@ const int KEY_LENGTH = 26;
 const string ERROR_LENGTH = "Error! The encryption key must contain 26 characters." ;
 const string LOWER_CASE = "Error! The encryption key must contain only lower case characters.";
 const string ERROR_ALPHABET = "Error! The encryption key must contain all alphabets a-z.";
-const string LOWER_CASE_ENCRKEY = "Error! The encryption key must contain all alphabets a-z.";
+const string LOWER_CASE_ENCRKEY = "Error! The text to be encrypted must contain all alphabets a-z.";
 
 bool check_length(string string_input) {
 
@@ -82,7 +82,6 @@ int main()
         return EXIT_FAILURE;
     }
 
-    ;
 
     cout << "Encrypted text: " << encryption(encr_key, text) << endl;
 
