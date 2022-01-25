00383f72781aaeb3ec3bac1081c9a9ec58fbaede
Terhi Rees <terhi.rees@tuni.fi>
2022-01-25 16:17:34 +0200

    some stuff done

---
+++ student/03/simple_library/book.hh
@@ -0,0 +1,28 @@
+#ifndef BOOK_HH
+#define BOOK_HH
+
+#include <iostream>
+
+using namespace std;
+
+class Book
+{
+public:
+    Book();
+
+    Book(string author, string bookname);
+
+
+    loan(Date&)
+    print()
+    renew()
+    give_back()
+
+private:
+
+    author_
+    bookname_
+
+};
+
+#endif // BOOK_HH
