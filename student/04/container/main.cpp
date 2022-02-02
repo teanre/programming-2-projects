1e30b88ef13032cecdc3828f7684c8d528fa86f3
Terhi Rees <terhi.rees@tuni.fi>
2022-02-02 18:44:26 +0200

    lots of fixes

--- student/04/container/main.cpp
+++ student/04/container/main.cpp
@@ -19,35 +19,36 @@ bool same_values(std::vector<int>& ints)
 {
     //onko kaikki alkiot samoja
 
-    for (unsigned long int index = 0; index < ints.size(); ++index)
+    int first_nr = ints.front();
+
+    for (int i : ints)
     {
-        if (ints.at(index) != ints.at(index+1))
-        {
-            return false;
-        }
-        else
-        {
-           return true;
-        }
+
+       if ( i != first_nr)
+       {
+           return false;
+       }
     }
+
     return true;
 }
 
 bool is_ordered_non_strict_ascending(std::vector<int>& ints)
 {
+
+    int previous_nr = ints.front();
+
    //onko seuraava alkio joko yhtä suuri tai suurempi kuin edellinen
-    for (unsigned long int index = 0; index < ints.size(); ++index)
+    for (int i : ints)
     {
-        if (ints.at(index+1) == ints.back())
-        {
-            break;
-        }
-        else if (ints.at(index) <= ints.at(index+1))
-        {
-            return true;
-        }
+       if (i < previous_nr)
+       {
+           return false;
+       }
+       previous_nr = i;
     }
-    return false;
+
+    return true;
 }
 
 bool is_arithmetic_series(std::vector<int>& ints)
@@ -55,46 +56,61 @@ bool is_arithmetic_series(std::vector<int>& ints)
     std::vector<int> difference;
 
     //jos kahden peräkkäisen alkion erotus on aina sama
-    for (unsigned long int index = 0; index < ints.size(); ++index)
+    for (unsigned int i = 0; i < ints.size(); ++i)
     {
-        if (ints.at(index+1) == ints.back())
+        if (ints.at(i) == ints.front())
         {
-            break;
+            int diff = (ints.at(i) - ints.at(i+1));
+            difference.push_back(diff);
+        }
+
+        if (ints.at(i) == ints.back())
+        {
+            int diff = (ints.at(i-1) - ints.at(i));
+            difference.push_back(diff);
         }
         else
         {
-            (difference.push_back(ints.at(index) - ints.at(index+1)));
-        }}
-
-
-    if (same_values(difference)){
-        return true;
+        difference.push_back(ints.at(i) - ints.at(i+1));
+        }
     }
-    return false;
+
+    if (!same_values(difference)){
+        return false;
+    }
+    return true;
 }
 
 bool is_geometric_series(std::vector<int>& ints)
 {
     //peräkkäisten termien suhdeluku vakio
 
+    // entäs jo kaikki luvut nolla
+
     std::vector<int> ratios;
 
     int ratio = 0;
 
-    for (unsigned int index = 0; index < (ints.size()-1); ++index){
-       if (ints.at(index +1) == ints.back()){
+    for (unsigned int index = 0; index < ints.size(); ++index)
+    {
+       if (ints.at(index) == ints.back())
+       {
            continue;
-       } else {
-        ratio = ints.at(index+1)/ints.at(index);
-        ratios.push_back(ratio);
-       }}
+       }
+       else
+       {
+            ratio = ints.at(index+1)/ints.at(index);
+            ratios.push_back(ratio);
+       }
+    }
 
-   if (same_values(ratios)){
+    if (same_values(ratios)){
             return true;
         } else {
-            return false;
-        }
-}
+
+         return false;
+
+}}
 
 int main()
 {
