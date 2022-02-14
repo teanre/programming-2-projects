f7e10b7c6dd322f265c46a9e6153209141169fa9
Terhi Rees <terhi.rees@tuni.fi>
2022-02-14 09:27:38 +0200

    ohjelma valmis

---
+++ student/05/line_numbers/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string input_file = "";
+    cout << "Input file: ";
+    getline(cin, input_file);
+
+    string output_file = "";
+    cout << "Output file: ";
+    getline(cin, output_file);
+
+    ifstream inputf(input_file);
+    ofstream outputf(output_file);
+    if (not inputf)
+    {
+        cout << "Error! The file " << input_file << " cannot be opened." << endl;
+    }
+    else
+    {
+        string row;
+        int row_number = 1;
+        while ( getline (inputf, row))
+        {
+            outputf << row_number << " " <<  row << endl;
+            row_number += 1;
+        }
+        inputf.close();
+        outputf.close();
+    }
+
+
+    return 0;
+}
