835775bcf085d3f31a3f4a0f10f988ae05a4f020
Terhi Rees <terhi.rees@tuni.fi>
2022-04-21 13:15:31 +0300

    gameboard ui done

--- student/12/numbers_gui/mainwindow.cpp
+++ student/12/numbers_gui/mainwindow.cpp
@@ -1,16 +1,28 @@
 #include "mainwindow.hh"
 #include "ui_mainwindow.h"
+#include "gameboard.hh"
+#include <QGraphicsProxyWidget>
 #include <QGraphicsView>
 #include <QGraphicsRectItem>
+#include <QGridLayout>
+#include <iostream>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui_(new Ui::MainWindow)
+    , scene_()
+    , rect_()
+    , graboard_()
+    , board_()
+    , labels_()
+    , board()
 {
     ui_->setupUi(this);
 
     scene_ = new QGraphicsScene(this);
+    //QGraphicsProxyWidget *proxy = new QGraphicsProxyWidget;
 
+    //QGraphicsProxyWidget *proxy = scene.addWidget(tabWidget);
     int left_margin = 10; // x coordinate
     int top_margin = 10; // y coordinate
         // The width of the graphicsView is BORDER_RIGHT added by 2,
@@ -18,33 +30,99 @@ MainWindow::MainWindow(QWidget *parent)
         // (1 on the left, and 1 on the right).
         // Similarly, the height of the graphicsView is BORDER_DOWN added by 2.
     ui_->graphicsView->setGeometry(left_margin, top_margin,
-                                       BORDER_RIGHT + 2, BORDER_DOWN + 2);
+                                       SIZE*STEP + 2, SIZE*STEP + 2);
     ui_->graphicsView->setScene(scene_);
 
-    // The width of the scene_ is BORDER_RIGHT decreased by 1 and
-        // the height of it is BORDER_DOWN decreased by 1,
-        // because the circle is considered to be inside the sceneRect,
-        // if its upper left corner is inside the sceneRect.
+    // The width of the scene_ is BORDER_RIGHT decreased by 1 an
 
+    scene_->setSceneRect(0, 0, SIZE*STEP - 1, SIZE*STEP - 1);
 
-    int x_coord = 0;
-    int y_coord = 0;
-    for (int i = 0; i < 20; ++i)
-    {
-        for ( int j = 0; j < 20; ++j)
-        {
-            scene_->setSceneRect(i, j, BORDER_RIGHT - 1, BORDER_DOWN - 1);
-            scene_->addRect(i, j, STEP, STEP);
-            ++x_coord;
-            ++y_coord;
-        }
-
-    }
+    createGraphicalBoard();
 
 }
 
 MainWindow::~MainWindow()
 {
     delete ui_;
+}
+
+void MainWindow::createGraphicalBoard()
+{
+
+    QBrush whiteBrush(Qt::white);
+    QPen blackPen(Qt::black);
+    blackPen.setWidth(2);
+
+    std::vector<QGraphicsRectItem*> rects;
+    //to change text content, can be reached by coordinates?
+    std::vector<QLabel*> helper;
+
+
+    for (int row = 0; row < SIZE; ++row)
+    {
+        for (int column = 0; column < SIZE; ++column)
+        {
+
+            QGraphicsRectItem* rect = new QGraphicsRectItem;
+            QLabel* label = new QLabel("*", this);
+            if (column == 0)
+            {
+                rect = scene_->addRect(column, row+(row*STEP), STEP, STEP, blackPen, whiteBrush);
+                label->setGeometry(column, row+(row*STEP), STEP, STEP);
+                label->setAlignment(Qt::AlignCenter);
+            }
+            else if (column >0)
+            {
+                rect = scene_->addRect(column+(column*STEP), row+(row*STEP), STEP, STEP, blackPen, whiteBrush);
+                label->setGeometry(column+(column*STEP), row+(row*STEP), STEP, STEP);
+                label->setAlignment(Qt::AlignCenter);
+            }
+            else
+            {
+               rect = scene_->addRect(column+(column*STEP), row, STEP, STEP, blackPen, whiteBrush);
+               label->setGeometry(column+(column*STEP), row, STEP, STEP);
+               label->setAlignment(Qt::AlignCenter);
+            }
+
+            helper.push_back(label);
+
+            rects.push_back(rect);
+
+            std::cout << "nyt on tehty näin monta rectii: " << rects.size() << std::endl;
+            std::cout << "labels pituus:" << labels_.size();
+
+        }
+        labels_.push_back(helper);
+           helper.clear();
+    }
+
+    labels_.at(2).at(2)->setText("HAHA");
+}
+
+/*void MainWindow::createButtonBoard()
+{
+    QGridLayout* grid = new QGridLayout;
+
+    for (int row = 0; row < SIZE; ++row)
+    {
+        for (int column = 0; column < SIZE; ++column)
+        {
+            QPushButton* button = new QPushButton;
+            button->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum);
+            button->setText("LA");
+            grid->addWidget(button, row, column);
+            board.append(button);
+        }
+    }
+    setLayout(grid);
+}*/
+
+
+
+
+void MainWindow::on_seedNumberBox_valueChanged(int arg1)
+{
+    //tää heittää gameboardille seedin ja täyttää
+    graboard_.fill(arg1);
 }
 
--- student/12/numbers_gui/mainwindow.hh
+++ student/12/numbers_gui/mainwindow.hh
@@ -1,10 +1,12 @@
 #ifndef MAINWINDOW_HH
 #define MAINWINDOW_HH
-
+#include "gameboard.hh"
 #include <QMainWindow>
 #include <QGraphicsView>
 #include <QGraphicsRectItem>
 #include <QGraphicsScene>
+#include <QPushButton>
+#include <QLabel>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -18,17 +20,28 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+
+    void on_seedNumberBox_valueChanged(int arg1);
+
 private:
     Ui::MainWindow *ui_;
 
     QGraphicsScene* scene_;
     QGraphicsRectItem* rect_;
-
-    const int STEP = 50;
+    GameBoard graboard_;
+    std::vector< std::vector< std::shared_ptr<NumberTile> > > board_;
+    std::vector<std::vector<QLabel*>> labels_;
+    QList<QPushButton*> board;
+    const int STEP = 100;
     const int BORDER_UP = 0;
     const int BORDER_DOWN = 260;
     const int BORDER_LEFT = 0;
     const int BORDER_RIGHT = 680;
+
+    void createGraphicalBoard();
+
+    //void createButtonBoard();
 
 };
 #endif // MAINWINDOW_HH
